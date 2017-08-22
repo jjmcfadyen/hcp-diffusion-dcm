e494b2e531dcca021012183af9786c5494836d67
Jessica McFadyen <j.mcfadyen@uq.edu.au>
2017-08-22 15:42:54 +1000

    Added noends, endsonly, and sift2
    Now computes noends (initial tckgen), endsonly (tckedit), and sift2 (just for endsonly - the noends is mainly for seeing where the tracks go to the rest of the brain, not so much to get and compare densities).

--- localtractography.sh
+++ localtractography.sh
@@ -11,33 +11,89 @@ dir_subject=INSERT
 dir_masks=${dir_subject}/warps/diffusion_space
 dir_script=INSERT
 
-mkdir -p ${dir_results}/ROI_SeedTractography/tckgen
-mkdir -p ${dir_results}/ROI_SeedTractography/tckgen_clusters
+mkdir -p ${dir_results}/ROI_SeedTractography/tckgen/noends
+mkdir -p ${dir_results}/ROI_SeedTractography/tckgen/endsonly
+mkdir -p ${dir_results}/ROI_SeedTractography/tckgen_clusters/noends
+mkdir -p ${dir_results}/ROI_SeedTractography/tckgen_clusters/endsonly
 
 cd ${dir_masks}
-printf "\n TCKGEN for ${subject}..."
+printf "\n TCKGEN, TCKEDIT, and SIFT for ${subject}..."
 for hemi in l r; do
+
 	printf "\n ... SC-PUL"
- 	time tckgen -seed_image SC_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz -include PUL_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen/SCPUL/SC-PUL_${hemi}_${subject}.tck
-	printf "\n ... PUL-AMY"
-	time tckgen -seed_image PUL_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz -include AMY_${hemi}_dwi_bin_${subject}.nii.gz -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen/PULAMY/PUL-AMY_${hemi}_${subject}.tck
+	mask1="SC_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz"
+	mask2="PUL_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz"
+	tracklabel="SC-PUL"
+
+	# note that this block is identical to the blocks below - no need to edit, should just need to edit the labels in the block above
+ 	time tckgen -seed_image ${mask1} -include ${mask2} -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen/noends/noends_${tracklabel}_${hemi}_${subject}.tck
+	time tckedit -include ${mask1} -include ${mask2} -ends_only ${dir_results}/ROI_SeedTractography/tckgen/noends/noends_${tracklabel}/${tracklabel}_${hemi}_${subject}.tck ${dir_results}/ROI_SeedTractography/tckgen/endsonly/endsonly_${tracklabel}_${hemi}_${subject}.tck
+	time tcksift2 -info -force -nthreads ${nthreads} -act ${dir_subject}/5TT_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen/endsonly/endsonly_${tracklabel}_${hemi}_${subject}.tck ${dir_subject}/WM_FODs.mif ${dir_results}/ROI_SeedTractography/tckgen/endsonly/sift2_endsonly_${tracklabel}_${hemi}_${subject}.txt
+	
 	printf "\n ... PUL-SC"
-	time tckgen -seed_image PUL_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz -include SC_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen/PULSC/PUL-SC_${hemi}_${subject}.tck
-	printf "\n ... AMY-PUL"
-	time tckgen -seed_image AMY_${hemi}_dwi_bin_${subject}.nii.gz -include PUL_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen/AMYPUL/AMY-PUL_${hemi}_${subject}.tck
+	mask1="PUL_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz"
+	mask2="SC_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz"
+	tracklabel="PUL-SC"
+
+	# note that this block is identical to the blocks below - no need to edit, should just need to edit the labels in the block above
+ 	time tckgen -seed_image ${mask1} -include ${mask2} -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen/noends/noends_${tracklabel}_${hemi}_${subject}.tck
+	time tckedit -include ${mask1} -include ${mask2} -ends_only ${dir_results}/ROI_SeedTractography/tckgen/noends/noends_${tracklabel}/${tracklabel}_${hemi}_${subject}.tck ${dir_results}/ROI_SeedTractography/tckgen/endsonly/endsonly_${tracklabel}_${hemi}_${subject}.tck
+	time tcksift2 -info -force -nthreads ${nthreads} -act ${dir_subject}/5TT_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen/endsonly/endsonly_${tracklabel}_${hemi}_${subject}.tck ${dir_subject}/WM_FODs.mif ${dir_results}/ROI_SeedTractography/tckgen/endsonly/sift2_endsonly_${tracklabel}_${hemi}_${subject}.txt
+	
+	printf "\n ... PUL-AMY"
+	mask1="PUL_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz"
+	mask2="AMY_${hemi}_dwi_bin_${subject}.nii.gz"
+	tracklabel="PUL-AMY"
+
+	# note that this block is identical to the blocks below - no need to edit, should just need to edit the labels in the block above
+ 	time tckgen -seed_image ${mask1} -include ${mask2} -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen/noends/noends_${tracklabel}_${hemi}_${subject}.tck
+	time tckedit -include ${mask1} -include ${mask2} -ends_only ${dir_results}/ROI_SeedTractography/tckgen/noends/noends_${tracklabel}/${tracklabel}_${hemi}_${subject}.tck ${dir_results}/ROI_SeedTractography/tckgen/endsonly/endsonly_${tracklabel}_${hemi}_${subject}.tck
+	time tcksift2 -info -force -nthreads ${nthreads} -act ${dir_subject}/5TT_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen/endsonly/endsonly_${tracklabel}_${hemi}_${subject}.tck ${dir_subject}/WM_FODs.mif ${dir_results}/ROI_SeedTractography/tckgen/endsonly/sift2_endsonly_${tracklabel}_${hemi}_${subject}.txt
+	
+	printf "\n ... PUL-AMY"
+	mask1="AMY_${hemi}_dwi_bin_${subject}.nii.gz"
+	mask2="PUL_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz"
+	tracklabel="AMY-PUL"
+
+	# note that this block is identical to the blocks below - no need to edit, should just need to edit the labels in the block above
+ 	time tckgen -seed_image ${mask1} -include ${mask2} -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen/noends/noends_${tracklabel}_${hemi}_${subject}.tck
+	time tckedit -include ${mask1} -include ${mask2} -ends_only ${dir_results}/ROI_SeedTractography/tckgen/noends/noends_${tracklabel}/${tracklabel}_${hemi}_${subject}.tck ${dir_results}/ROI_SeedTractography/tckgen/endsonly/endsonly_${tracklabel}_${hemi}_${subject}.tck
+	time tcksift2 -info -force -nthreads ${nthreads} -act ${dir_subject}/5TT_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen/endsonly/endsonly_${tracklabel}_${hemi}_${subject}.tck ${dir_subject}/WM_FODs.mif ${dir_results}/ROI_SeedTractography/tckgen/endsonly/sift2_endsonly_${tracklabel}_${hemi}_${subject}.txt
+	
 done
 
-printf "\n TCKGEN CLUSTERS for ${subject}..."
+printf "\n TCKGEN, TCKEDIT, and SIFT CLUSTERS for ${subject}..."
 for cluster in 1 2 3 4 5; do
 	for hemi in l r; do
+
 		printf "\n ... SC-PUL"
-	 	time tckgen -seed_image SC_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz -include PUL_${hemi}_Cluster${cluster}_dwi_bin_no-overlap_noclusteroverlap_${subject}.nii.gz -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen_clusters/SC-PUL_${hemi}_${subject}.tck
-		printf "\n ... PUL-AMY"
-		time tckgen -seed_image PUL_${hemi}_Cluster${cluster}_dwi_bin_no-overlap_noclusteroverlap_${subject}.nii.gz -include AMY_${hemi}_dwi_bin_${subject}.nii.gz -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen_clusters/PUL-AMY_${hemi}_${subject}.tck
+		mask1="SC_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz"
+		mask2="PUL_${hemi}_Cluster${cluster}_dwi_bin_no-overlap_noclusteroverlap_${subject}.nii.gz"
+		tracklabel="SC-PUL_Cluster{cluster}"
+	 	time tckgen -seed_image ${mask1} -include ${mask2} -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen_clusters/noends/noends_${tracklabel}_${hemi}_${subject}.tck
+		time tckedit -include ${mask1} -include ${mask2} -ends_only ${dir_results}/ROI_SeedTractography/tckgen/noends/noends_${tracklabel}/${tracklabel}_${hemi}_${subject}.tck ${dir_results}/ROI_SeedTractography/tckgen_clusters/endsonly/endsonly_${tracklabel}_${hemi}_${subject}.tck
+		
 		printf "\n ... PUL-SC"
-		time tckgen -seed_image PUL_${hemi}_Cluster${cluster}_dwi_bin_no-overlap_noclusteroverlap_${subject}.nii.gz -include SC_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen_clusters/PUL-SC_${hemi}_${subject}.tck
+		mask1="PUL_${hemi}_Cluster${cluster}_dwi_bin_no-overlap_noclusteroverlap_${subject}.nii.gz"
+		mask2="SC_${hemi}_dwi_bin_no-overlap_${subject}.nii.gz"
+		tracklabel="PUL-SC_Cluster{cluster}"
+	 	time tckgen -seed_image ${mask1} -include ${mask2} -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen_clusters/noends/noends_${tracklabel}_${hemi}_${subject}.tck
+		time tckedit -include ${mask1} -include ${mask2} -ends_only ${dir_results}/ROI_SeedTractography/tckgen/noends/noends_${tracklabel}/${tracklabel}_${hemi}_${subject}.tck ${dir_results}/ROI_SeedTractography/tckgen_clusters/endsonly/endsonly_${tracklabel}_${hemi}_${subject}.tck
+		
+		printf "\n ... PUL-AMY"
+		mask1="PUL_${hemi}_Cluster${cluster}_dwi_bin_no-overlap_noclusteroverlap_${subject}.nii.gz"
+		mask2="AMY_${hemi}_dwi_bin_${subject}.nii.gz"
+		tracklabel="PUL-AMY_Cluster{cluster}"
+	 	time tckgen -seed_image ${mask1} -include ${mask2} -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen_clusters/noends/noends_${tracklabel}_${hemi}_${subject}.tck
+		time tckedit -include ${mask1} -include ${mask2} -ends_only ${dir_results}/ROI_SeedTractography/tckgen/noends/noends_${tracklabel}/${tracklabel}_${hemi}_${subject}.tck ${dir_results}/ROI_SeedTractography/tckgen_clusters/endsonly/endsonly_${tracklabel}_${hemi}_${subject}.tck
+		
 		printf "\n ... AMY-PUL"
-		time tckgen -seed_image AMY_${hemi}_dwi_bin_${subject}.nii.gz -include PUL_${hemi}_Cluster${cluster}_dwi_bin_no-overlap_noclusteroverlap_${subject}.nii.gz -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen_clusters/AMY-PUL_${hemi}_${subject}.tck
+		mask1="AMY_${hemi}_dwi_bin_${subject}.nii.gz"
+		mask2="PUL_${hemi}_Cluster${cluster}_dwi_bin_no-overlap_noclusteroverlap_${subject}.nii.gz"
+		tracklabel="PAMY-PUL_Cluster{cluster}"
+	 	time tckgen -seed_image ${mask1} -include ${mask2} -stop -act ${dir_subject}/5TT_${subject}.mif -crop_at_gmwmi -number 10000 -maxnum 25000 -cutoff 0.06 -force -nthreads ${nthreads} ${dir_subject}/WM_FODs_${subject}.mif ${dir_results}/ROI_SeedTractography/tckgen_clusters/noends/noends_${tracklabel}_${hemi}_${subject}.tck
+		time tckedit -include ${mask1} -include ${mask2} -ends_only ${dir_results}/ROI_SeedTractography/tckgen/noends/noends_${tracklabel}/${tracklabel}_${hemi}_${subject}.tck ${dir_results}/ROI_SeedTractography/tckgen_clusters/endsonly/endsonly_${tracklabel}_${hemi}_${subject}.tck
+		
 	done
 done
 
