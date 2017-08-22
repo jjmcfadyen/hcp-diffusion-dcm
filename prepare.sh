41df83d94cdbfc4ebc2234c329cb084954cf4387
Jessica McFadyen <j.mcfadyen@uq.edu.au>
2017-08-22 14:41:26 +1000

    Subject number as suffix
    Moved the subject number to the end of input/output files

--- prepare.sh
+++ prepare.sh
@@ -16,16 +16,16 @@ mkdir -p ${dir_subject}
 ######## COMMANDS ########
 cd ${dir_hcp}
 echo "Using FSL to fill holes in ${subject}'s nodif_brain_mask..."
-time fslmaths nodif_brain_mask.nii.gz -fillh -bin -thr .0000000001 ${dir_subject}/${subject}_nodif_brain_mask_fillh.nii.gz
+time fslmaths nodif_brain_mask.nii.gz -fillh -bin -thr .0000000001 ${dir_subject}/nodif_brain_mask_fillh_${subject}.nii.gz
 
 echo "Running dwibasicorrect for ${subject}..."
-time dwibiascorrect -ants -mask ${dir_subject}/${subject}_nodif_brain_mask_fillh.nii.gz -tempdir ${dir_tmp} -fslgrad bvecs bvals -force -verbose -nthreads 12 data.nii.gz ${dir_subject}/${subject}_cDWI.mif
+time dwibiascorrect -ants -mask ${dir_subject}/nodif_brain_mask_fillh_${subject}.nii.gz -tempdir ${dir_tmp} -fslgrad bvecs bvals -force -verbose -nthreads 12 data.nii.gz ${dir_subject}/cDWI_${subject}.mif
 echo "DWI Bias correction complete for ${subject}!"
 
 echo "Computing tensors for ${subject}..."
 cd ${dir_subject}
-time dwi2tensor -mask ${subject}_nodif_brain_mask_fillh.nii.gz -force -nthreads 12 -info ${subject}_cDWI.mif ${subject}_DT.mif
-time tensor2metric -fa ${subject}_FA.mif -force -nthreads 12 -info ${subject}_DT.mif
+time dwi2tensor -mask nodif_brain_mask_fillh_${subject}.nii.gz -force -nthreads 12 -info cDWI_${subject}.mif DT_${subject}.mif
+time tensor2metric -fa FA_${subject}.mif -force -nthreads 12 -info DT_${subject}.mif
 
 cd ${dir_script}
 echo "Finished!"
