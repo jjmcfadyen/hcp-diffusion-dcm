8854dbdab42d257ef4673d577f7fc0c0a2b2ccf0
Jessica McFadyen <j.mcfadyen@uq.edu.au>
2017-08-22 12:06:36 +1000

    Update prepare.sh
    Added in the "999999" code to prefix of all output files.

--- prepare.sh
+++ prepare.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 
-subject=100206
+subject=999999
 
 ######## DIRECTORIES ########
 # Home directory where this script is located
-dir_script=/ibscratch/users/uqjmcfad/DWI_AllSubjects/Scripts/01_prepare
+dir_script=INSERT
 # Temporary directory
-dir_tmp=/ibscratch/users/uqjmcfad/DWI_AllSubjects/tmp
+dir_tmp=INSERT
 # Data directory containing the data.nii.gz and nodif_brain_mask.nii.gz files
-dir_hcp=/ibscratch/users/uqjmcfad/DWI_AllSubjects/Scripts/01_prepare/errors/${subject}
+dir_hcp=INSERT
 # Subject-specific output directory
-dir_subject=/ibscratch/users/uqjmcfad/DWI_AllSubjects/Scripts/01_prepare/errors/${subject}
+dir_subject=INSERT/${subject}
 mkdir -p ${dir_subject}
 
 ######## COMMANDS ########
 cd ${dir_hcp}
 echo "Using FSL to fill holes in ${subject}'s nodif_brain_mask..."
-time fslmaths nodif_brain_mask.nii.gz -fillh -bin -thr .0000000001 ${dir_subject}/nodif_brain_mask_fillh.nii.gz
+time fslmaths nodif_brain_mask.nii.gz -fillh -bin -thr .0000000001 ${dir_subject}/${subject}_nodif_brain_mask_fillh.nii.gz
 
 echo "Running dwibasicorrect for ${subject}..."
-time dwibiascorrect -ants -mask ${dir_subject}/nodif_brain_mask_fillh.nii.gz -tempdir ${dir_tmp} -fslgrad bvecs bvals -force -verbose -nthreads 12 data.nii.gz ${dir_subject}/cDWI.mif
+time dwibiascorrect -ants -mask ${dir_subject}/${subject}_nodif_brain_mask_fillh.nii.gz -tempdir ${dir_tmp} -fslgrad bvecs bvals -force -verbose -nthreads 12 data.nii.gz ${dir_subject}/${subject}_cDWI.mif
 echo "DWI Bias correction complete for ${subject}!"
 
 echo "Computing tensors for ${subject}..."
 cd ${dir_subject}
-time dwi2tensor -mask nodif_brain_mask_fillh.nii.gz -force -nthreads 12 -info cDWI.mif DT.mif
-time tensor2metric -fa FA.mif -force -nthreads 12 -info DT.mif
+time dwi2tensor -mask ${subject}_nodif_brain_mask_fillh.nii.gz -force -nthreads 12 -info ${subject}_cDWI.mif ${subject}_DT.mif
+time tensor2metric -fa ${subject}_FA.mif -force -nthreads 12 -info ${subject}_DT.mif
 
 cd ${dir_script}
 echo "Finished!"
