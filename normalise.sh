62b082d1010a94c4e46dd5b7dc4b9daf1bfb25f1
Jessica McFadyen <j.mcfadyen@uq.edu.au>
2017-08-22 14:40:22 +1000

    Subject number as suffix
    Moved the subject number to the end of input/output files

--- normalise.sh
+++ normalise.sh
@@ -7,14 +7,13 @@ dir_subject=INSERT/${subject}
 dir_tmp=INSERT
 
 echo "mrregister: ${subject}"
-time mrregister ${dir_script}/FA_template.mif ${dir_subject}/999999_FA.mif -mask2 ${dir_subject}/999999_nodif_brain_mask_fillh.nii.gz -nl_scale 0.5,0.75,1.0 -nl_niter 5,5,15 -nl_warp ${dir_subject}/999999_FA.mif ${dir_subject}/tmp.mif -force -nthreads 4 -info
+time mrregister ${dir_script}/FA_template.mif ${dir_subject}/FA_${subject}.mif -mask2 ${dir_subject}/nodif_brain_mask_fillh_${subject}.nii.gz -nl_scale 0.5,0.75,1.0 -nl_niter 5,5,15 -nl_warp ${dir_subject}/FA_${subject}.mif ${dir_subject}/tmp.mif -force -nthreads 4 -info
 echo "mrtransform: ${subject}"
-time mrtransform ${dir_script}/WM_mask.mif -template ${dir_subject}/999999_cDWI.mif -warp ${dir_subject}/999999_FA.mif ${dir_subject}/transformed.mif -force -nthreads 4 -info
+time mrtransform ${dir_script}/WM_mask.mif -template ${dir_subject}/cDWI_${subject}.mif -warp ${dir_subject}/FA_${subject}.mif ${dir_subject}/transformed.mif -force -nthreads 4 -info
 echo "dwinormalise: ${subject}"
-time dwinormalise ${dir_subject}/999999_cDWI.mif ${dir_subject}/transformed.mif ${dir_subject}/999999_nDWI.mif -force -nthreads 4 -info
+time dwinormalise ${dir_subject}/cDWI_${subject}.mif ${dir_subject}/transformed.mif ${dir_subject}/nDWI_${subject}.mif -force -nthreads 4 -info
 
 echo "Finished!"
-echo ${subject} >> finished.txt
 echo "Cleaning up files..."
 rm ${dir_subject}/transformed.mif
 rm ${dir_subject}/tmp.mif
