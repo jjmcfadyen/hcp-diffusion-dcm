d4b84e4b3682fe92bec8ef9fd5c5b9560e02ffa2
Jessica McFadyen <j.mcfadyen@uq.edu.au>
2017-08-23 17:57:39 +1000

    Update sumGlobal.sh

--- sumGlobal.sh
+++ sumGlobal.sh
@@ -1,7 +1,7 @@
-## Get streamline count for global tracks
+## Get streamline count for track types
 dir_results=/projects/ap66/uqjmcfad/HCP_SubcorticalRoute/Results/Global
 for tcktype in SC-PUL PUL-AMY; do
-	cd ${dir_results}/${tcktype}
+	cd ${dir_results}/endsonly/${tcktype}
 	rm -rf ${dir_results}/${tcktype}/count_*
 	for i in *.tck; do
 		filename=$(basename ${i} .tck)
@@ -10,6 +10,6 @@ for tcktype in SC-PUL PUL-AMY; do
 		subject=$(echo $filename | egrep -o [0-9]+ | tail -c 7)
 		tckname=$(basename ${i} _${subject}.tck)
 		echo "Writing streamline count ${count} for ${tckname}, ${subject}..."
-		echo "$subject $count" >> ${dir_results}/${tcktype}/count_${tckname}.txt
+		echo "$subject $count" >> ${dir_results}/endsonly/${tcktype}/count_${tckname}.txt
 	done
 done
